b82f25cda76254201e1c426015c9a58b6db18b2f
Patricia Castro <patricia.castro@fabamaq.com>
2019-09-20 11:23:19 +0100

    Added 3 new tests for search terms

--- tests/filterByTerm.spec.js
+++ tests/filterByTerm.spec.js
@@ -1,3 +1,43 @@
+
+const filterByTerm = require("../src/filterByTerm");
+
 describe("Filter function", () => {
-	//test
+	test("it should filter by a search term (link)", () => {
+		const input = [
+			{ id: 1, url: "https://www.url1.dev" },
+			{ id: 2, url: "https://www.url2.dev" },
+			{ id: 3, url: "https://www.link3.dev" }
+		];
+		const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+		expect(filterByTerm(input, "link")).toEqual(output);
+		expect(filterByTerm(input, "LINK")).toEqual(output);
+	});
+});
+
+describe("Filter function", () => {
+	test("it should filter by a search term (uRl)", () => {
+		const input = [
+			{ id: 1, url: "https://www.url1.dev" },
+			{ id: 2, url: "https://www.url2.dev" },
+			{ id: 3, url: "https://www.link3.dev" }
+		];
+		const output = [{ id: 1, url: "https://www.url1.dev" },
+		{ id: 2, url: "https://www.url2.dev" }];
+
+		expect(filterByTerm(input, "uRl")).toEqual(output);
+	});
+});
+
+describe("Filter function", () => {
+	test("it should filter by an empty search term", () => {
+		const input = [
+			{ id: 1, url: "https://www.url1.dev" },
+			{ id: 2, url: "https://www.url2.dev" },
+			{ id: 3, url: "https://www.link3.dev" }
+		];
+		const output = [];
+
+		expect(filterByTerm(input, " "));
+	});
 });
